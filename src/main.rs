3f3ae52f5cd65809287c7d532e862f2b08672c03
k-nasa <htilcs1115@gmail.com>
2019-11-09 09:52:42 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use env_logger as logger;
 use log::{debug, info};
 
 // TODO: change to command line arguments
-const MAX_CONNECTIONS: usize = 100;
+const MAX_CONNECTIONS: usize = 10;
 const REQUEST_AMMOUNT: usize = 10_000;
 
 fn main() -> std::io::Result<()> {
@@ -47,11 +47,10 @@ fn main() -> std::io::Result<()> {
 async fn send_request() -> Result<(), async_std::io::Error> {
     let mut stream = TcpStream::connect("127.0.0.1:8080").await?;
 
-    stream.write_all(b"GET / HTTP/1.1\n").await?;
-    stream.write_all(b"Host: localhost:8080\n").await?;
-    stream.write_all(b"User-Agent: goku/0.0.1\n").await?;
-    stream.write_all(b"Accept: */*\n").await?;
-    stream.write_all(b"\n").await?;
+    const HTTP_REQUEST: &'static [u8] =
+        b"GET / HTTP/1.1\nHost: localhost:8080\nUser-Agent: goku/0.0.1\n\n";
+
+    stream.write_all(HTTP_REQUEST).await?;
 
     Ok(())
 }
