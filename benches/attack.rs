5baa05db8efe064c2614e0ca51507f80cc5feb36
k-nasa <htilcs1115@gmail.com>
2019-11-10 08:08:36 +0900

    Add attack command bench

---
+++ benches/attack.rs
@@ -0,0 +1,43 @@
+#![feature(test)]
+
+extern crate test;
+
+use goku::attack;
+use goku::send_request;
+use test::Bencher;
+
+#[bench]
+fn attack_bench(b: &mut Bencher) {
+    b.iter(|| {
+        attack(1, 1, &"127.0.0.1", 8080).unwrap();
+    });
+}
+
+#[bench]
+fn send_request_bench(b: &mut Bencher) {
+    let request = format!(
+        "GET / HTTP/1.1\nHost: {}\nUser-Agent: goku/0.0.1\n\n",
+        "localhost:8080"
+    );
+
+    let host = format!("{}:{}", "127.0.0.1", "8080");
+    b.iter(|| {
+        async_std::task::block_on(async {
+            send_request(&host, &request).await.unwrap();
+        })
+    });
+}
+
+#[bench]
+fn attack_bench_50_request_10_concurrency(b: &mut Bencher) {
+    b.iter(|| {
+        attack(10, 50, &"127.0.0.1", 8080).unwrap();
+    });
+}
+
+#[bench]
+fn attack_bench_when_50_request_1_concurrency(b: &mut Bencher) {
+    b.iter(|| {
+        attack(1, 50, &"127.0.0.1", 8080).unwrap();
+    });
+}
